0cdeb0cbd96482b1635bfe12fe1809e8fc8c2b6e
Phong Nguyen <84949014+Baegilpong@users.noreply.github.com>
2021-06-02 16:50:03 -0400

    Update iostream.cpp

--- chapter6/iostream.cpp
+++ chapter6/iostream.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 using namespace std;
 
 int main()
@@ -14,8 +15,17 @@ int main()
     ofstream out_stream;
 
     in_stream.open("infile.txt");
+    if(in_stream.fail())
+    {
+        cout << "Input file opening failed \n";
+        exit(1);
+    }
     out_stream.open("outfile.txt");
-
+    if(out_stream.fail())
+    {
+        cout << "Output file opening failed \n";
+        exit(1);
+    }
     int num1, num2, num3;
 
     in_stream >> num1 >> num2 >> num3;
@@ -23,8 +33,9 @@ int main()
     out_stream << "first number = " << num1
                << "\nsecond number = " << num2
                << "\nthird number = " << num3;
-    
+               
     in_stream.close();
     out_stream.close();
+
     return 0;
 }
